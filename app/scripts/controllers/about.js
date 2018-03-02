80a8e038135102637ece359c4284df6292de675a
Matt Hedlund <matthewrhedlund@gmail.com>
2018-03-02 15:40:08 -0500

    feat: Initial project commit

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name secretCipherApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the secretCipherApp
+ */
+angular.module('secretCipherApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name secretCipherApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the secretCipherApp
+ */
+angular.module('secretCipherApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
