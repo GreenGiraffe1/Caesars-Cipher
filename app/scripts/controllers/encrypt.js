0e778e963e881236401c0b04af9935593803725d
Matt Hedlund <matthewrhedlund@gmail.com>
2018-03-03 16:43:58 -0500

    feat: Add view & controller for message encryption

---
+++ app/scripts/controllers/encrypt.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name secretCipherApp.controller:EncryptCtrl
+ * @description
+ * # EncryptCtrl
+ * Controller of the secretCipherApp
+ */
+angular.module('secretCipherApp')
+  .controller('EncryptCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
