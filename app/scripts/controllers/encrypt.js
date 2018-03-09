8bdeaa1493a81bc2fd712daa4e2c0bbf21e1be23
Matt Hedlund <matthewrhedlund@gmail.com>
2018-03-09 15:32:29 -0500

    feat: Remove old code & unify spacing

--- app/scripts/controllers/encrypt.js
+++ app/scripts/controllers/encrypt.js
@@ -8,78 +8,68 @@
  * Controller of the secretCipherApp
  */
 angular.module('secretCipherApp')
-  .controller('EncryptCtrl', function () {
-    this.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
-    // this.messageComponents = {
-    //     'subject' : '';
-    //     'body' : '';
-    //     'passcode' : '';
-    //     'algorithm' : '';
-    // };
-    this.subject = '[type subject here]';
-    this.message = '[type message here]';
-    this.algorithm = null;
-    this.testFunc = function() {
-        // alert('testFunc fired!!');
-        console.log('testFunc fired!!');
-        console.log(this.subject);
-    };
-    // this.charShift = 0;
-    this.shift = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26];
-    this.caesar = function(string,shift) {
-        
-        var stringArray = string.split('');
-        var newStringArray = [];
-        
-        // TAKE CARE OF NEGATIVE SHIFTS HERE !!!!!!!!  (input will be restricted to 1-26)
-        if (shift < 0) {
-            shift = 26 + shift;
-        }
-        
-        // Iterates over and prints all letters of string 
-        for (var i = 0; i < stringArray.length; i++) {
+    .controller('EncryptCtrl', function () {
+        this.subject = '[type subject here]';
+        this.message = '[type message here]';
+        this.algorithm = null;
+        this.testFunc = function() {
+            // alert('testFunc fired!!');
+            console.log('testFunc fired!!');
+            console.log(this.subject);
+        };
+        // this.charShift = 0;
+        this.shift = [1,2,3
+            ,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26];
+        this.caesar = function(string,shift) {
             
-            var oldValue = stringArray[i];
-            var oldAsciiNum = oldValue.charCodeAt();
-            //  Set base-case for new ASCII code
-            var newAsciiNum = 0;
+            var stringArray = string.split('');
+            var newStringArray = [];
             
-            // Transform Upper-Case characters
-            if (91 > oldAsciiNum && oldAsciiNum > 64) {
-                if ((oldAsciiNum + shift) > 90) {
-                    newAsciiNum = 64 + ((oldAsciiNum + shift) % 90);
-                } else {
-                    newAsciiNum = oldAsciiNum + shift;
-                }
+            // TAKE CARE OF NEGATIVE SHIFTS HERE !!!!!!!!  (input will be restricted to 1-26)
+            if (shift < 0) {
+                shift = 26 + shift;
             }
             
-            // Transform Lower-Case characters
-            if (123 > oldAsciiNum && oldAsciiNum > 96) {
-                if ((oldAsciiNum + shift) > 122) {
-                    newAsciiNum = 96 + ((oldAsciiNum + shift) % 122);
+            // Iterates over and prints all letters of string 
+            for (var i = 0; i < stringArray.length; i++) {
+                
+                var oldValue = stringArray[i];
+                var oldAsciiNum = oldValue.charCodeAt();
+                //  Set base-case for new ASCII code
+                var newAsciiNum = 0;
+                
+                // Transform Upper-Case characters
+                if (91 > oldAsciiNum && oldAsciiNum > 64) {
+                    if ((oldAsciiNum + shift) > 90) {
+                        newAsciiNum = 64 + ((oldAsciiNum + shift) % 90);
+                    } else {
+                        newAsciiNum = oldAsciiNum + shift;
+                    }
+                }
+                
+                // Transform Lower-Case characters
+                if (123 > oldAsciiNum && oldAsciiNum > 96) {
+                    if ((oldAsciiNum + shift) > 122) {
+                        newAsciiNum = 96 + ((oldAsciiNum + shift) % 122);
+                    } else {
+                        newAsciiNum = oldAsciiNum + shift;
+                    }
+                }
+
+                // Pass through non-alphabetic characters without change
+                if (newAsciiNum !== 0) {
+                    var newValue = String.fromCharCode(newAsciiNum);
+                    newStringArray.push(newValue);
                 } else {
-                    newAsciiNum = oldAsciiNum + shift;
+                    newStringArray.push(oldValue);
                 }
             }
 
-            // Pass through non-alphabetic characters without change
-            if (newAsciiNum !== 0) {
-                var newValue = String.fromCharCode(newAsciiNum);
-                newStringArray.push(newValue);
-            } else {
-                newStringArray.push(oldValue);
-            }
-        }
-
-        // Join the array of new letters into the resultant string
-        var newString = newStringArray.join('');
-        alert(newString);
-        return newString;
-    
+            // Join the array of new letters into the resultant string
+            var newString = newStringArray.join('');
+            alert(newString);
+            return newString;
         
-    }
-  });
+            
+        }
+    });
