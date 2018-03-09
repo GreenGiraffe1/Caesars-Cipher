b2cc4d0d9b5cefbd766f2ce7dc7a9f76a637c564
Matt Hedlund <matthewrhedlund@gmail.com>
2018-03-09 15:58:12 -0500

    feat: Update logic of select-options form element

--- app/scripts/controllers/encrypt.js
+++ app/scripts/controllers/encrypt.js
@@ -20,6 +20,7 @@ angular.module('secretCipherApp')
         // this.charShift = 0;
         this.shift = [1,2,3
             ,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26];
+        this.charShift = null;
         this.caesar = function(string,shift) {
             
             var stringArray = string.split('');
