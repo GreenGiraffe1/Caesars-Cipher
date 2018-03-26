bac238669ef74aabb8eab05cd9cb41246d817ea3
Matt Hedlund <matthewrhedlund@gmail.com>
2018-03-26 07:58:39 -0400

    feat: Add state variable to track whether the message is encrypted

--- app/scripts/controllers/encrypt.js
+++ app/scripts/controllers/encrypt.js
@@ -14,6 +14,7 @@ angular.module('secretCipherApp')
         this.shift = [1,2,3
             ,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26];
         this.charShift = 23;
+        this.cryptState = 0;  //  The default is False
         this.toggleCrypt = function(num) {
             if (this.message === '') {
                 alert('Must enter a message before taking that action.');
