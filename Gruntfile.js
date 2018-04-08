194fe56bd2d7cb45436854c0cfdd609dd0b525de
Matt Hedlund <matthewrhedlund@gmail.com>
2018-04-08 15:19:34 -0400

    fix: repair filename

--- Gruntfile.js
+++ Gruntfile.js
@@ -338,7 +338,7 @@ module.exports = function (grunt) {
     ngtemplates: {
       dist: {
         options: {
-          module: 'caesarsCipherApp',
+          module: 'secretCipherApp',
           htmlmin: '<%= htmlmin.dist.options %>',
           usemin: 'scripts/scripts.js'
         },
